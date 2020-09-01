031fdf5c6549984706c9c06c2172baa89061ce4f
Said Shah <sshah@andculture.com>
2020-09-01 14:38:35 -0400

    Added Block and Blockchain classes

---
+++ index.js
@@ -0,0 +1,42 @@
+const hashWithSha256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = "") {
+        this.data         = data;
+        this.index        = index;
+        this.previousHash = previousHash;
+        this.timestamp    = timestamp;
+        this.hash         = this.calculateHash();
+    }
+
+    calculateHash() {
+        const data = this.index + this.previousHash + this.timestamp + JSON.stringify(this.data);
+        return hashWithSha256(data).toString();
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenisisBlock()];
+    }
+
+    createGenisisBlock() {
+        return new Block(0, "08/01/2020", "Genesis Block", null);
+    }
+
+    getLastBlock() {
+        return this.chain[this.chain.length -1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLastBlock().hash;
+        newBlock.hash         = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let blockchain = new Blockchain();
+blockchain.addBlock(new Block(1, "08/23/2020", { coins: 23 }));
+blockchain.addBlock(new Block(2, "08/29/2020", { coins: 11 }));
+
+console.log(JSON.stringify(blockchain, null, 2))
